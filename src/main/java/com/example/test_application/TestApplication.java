e2b6af350fda907627b748734213bacedd4caaea
davibss <davi.sousa@ccc.ufcg.edu.br>
2022-07-08 17:47:11 -0300

    adding new changes

--- src/main/java/com/example/test_application/TestApplication.java
+++ src/main/java/com/example/test_application/TestApplication.java
@@ -1,9 +1,11 @@
 package com.example.test_application;
 
+import metrics_library.controllers.MetricsController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class TestApplication {
 
 	public static void main(String[] args) {
